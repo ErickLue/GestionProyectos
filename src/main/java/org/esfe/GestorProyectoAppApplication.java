9ebc65c4b482b23ac3ccca62f52823da0c962565
Isaiaslue42637 <Isaiaslue42637@outlook.com>
2024-08-06 16:41:40 -0600

    estructura de proyecto

---
+++ src/main/java/org/esfe/GestorProyectoAppApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorProyectoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorProyectoAppApplication.class, args);
+	}
+
+}
