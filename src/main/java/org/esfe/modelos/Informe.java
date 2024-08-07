207dfb7d509060ee6bd5f228ac5a5b5be0d9ba71
EnriqueEsquia1212 <EnriqueEsquia1212@outlook.es>
2024-08-07 11:47:14 -0600

    infos

--- src/main/java/org/esfe/modelos/Informe.java
+++ src/main/java/org/esfe/modelos/Informe.java
@@ -9,6 +9,62 @@ import java.util.Date;
 @Entity
 @Table(name = "Informes")
 public class Informe {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "El campo es requerido") String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(@NotBlank(message = "El campo es requerido") String nombre) {
+        this.nombre = nombre;
+    }
+
+    public @NotBlank(message = "El campo es requerido") Date getFechaIncio() {
+        return fechaIncio;
+    }
+
+    public void setFechaIncio(@NotBlank(message = "El campo es requerido") Date fechaIncio) {
+        this.fechaIncio = fechaIncio;
+    }
+
+    public @NotBlank(message = "El campo es requerido") Date getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(@NotBlank(message = "El campo es requerido") Date fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public LocalDate getEstadoId() {
+        return estadoId;
+    }
+
+    public void setEstadoId(LocalDate estadoId) {
+        this.estadoId = estadoId;
+    }
+
+    public int getProyectoId() {
+        return proyectoId;
+    }
+
+    public void setProyectoId(int proyectoId) {
+        this.proyectoId = proyectoId;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
