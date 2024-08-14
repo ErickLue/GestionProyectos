9cfa10ef1f702befb047650910300e2532bca72b
Isaiaslue42637 <Isaiaslue42637@outlook.com>
2024-08-14 11:35:07 -0600

    yyy

--- src/main/java/org/esfe/controladores/TareaController.java
+++ src/main/java/org/esfe/controladores/TareaController.java
@@ -1,6 +1,7 @@
 package org.esfe.controladores;
 
 import org.esfe.modelos.Miembro;
+import org.esfe.modelos.Proyecto;
 import org.esfe.modelos.Tarea;
 import org.esfe.servicios.interfaces.ITareaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -42,5 +43,50 @@ public class TareaController {
         }
 
         return "tarea/index";
+    }
+    @GetMapping("/create")
+    public String create(Tarea tarea){
+        return "tarea/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Tarea tarea, BindingResult result, Model model, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            model.addAttribute(tarea);
+            attributes.addFlashAttribute("error", "No se pudo guardar debido a un error.");
+            return "tarea/create";
+        }
+
+        tareaService.crearOEditar(tarea);
+        attributes.addFlashAttribute("msg", "tarea creada correctamente");
+        return "redirect:/tareas";
+    }
+
+    @GetMapping("/details/{id}")
+    public String details(@PathVariable("id") Integer id, Model model){
+        Tarea tarea = tareaService.buscarPorId(id).get();
+        model.addAttribute("tarea", tarea);
+        return "tarea/details";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model){
+        Tarea tarea = tareaService.buscarPorId(id).get();
+        model.addAttribute("tarea", tarea);
+        return "tarea/edit";
+    }
+
+    @GetMapping("/remove/{id}")
+    public String remove(@PathVariable("id") Integer id, Model model){
+        Tarea tarea = tareaService.buscarPorId(id).get();
+        model.addAttribute("tarea", tarea);
+        return "tarea/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Tarea tarea, RedirectAttributes attributes){
+        tareaService.eliminarPorid(tarea.getTarea_id());
+        attributes.addFlashAttribute("msg", "tarea ha sido eliminada correctamente");
+        return "redirect:/tarea";
     }
 }
