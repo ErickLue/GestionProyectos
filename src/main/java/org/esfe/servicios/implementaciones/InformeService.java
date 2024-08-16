28f7ac6b1a1bff5c59c13c594d320f5f287a0a29
EnriqueEsquia1212 <EnriqueEsquia1212@outlook.es>
2024-08-16 10:46:35 -0600

    vistas

--- src/main/java/org/esfe/servicios/implementaciones/InformeService.java
+++ src/main/java/org/esfe/servicios/implementaciones/InformeService.java
@@ -1,15 +1,17 @@
 package org.esfe.servicios.implementaciones;
 
 import org.esfe.modelos.Informe;
+import org.esfe.modelos.Miembro;
 import org.esfe.repositorio.IInformeRepository;
 import org.esfe.servicios.interfaces.IInformeServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class InformeService implements IInformeServices {
     @Autowired
     private IInformeRepository informeRepository;
@@ -28,7 +30,6 @@ public class InformeService implements IInformeServices {
     public Optional<Informe> buscarPorId(Integer informeId) {
         return informeRepository.findById(informeId);
     }
-
     @Override
     public Informe crearOEditar(Informe informe) {
         return informeRepository.save(informe);
