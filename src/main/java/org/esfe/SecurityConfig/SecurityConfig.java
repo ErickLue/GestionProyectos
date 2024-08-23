d62497c015869e4065238e6710214e41afe6d956
Isaiaslue42637 <Isaiaslue42637@outlook.com>
2024-08-23 10:18:43 -0600

    hola

--- src/main/java/org/esfe/SecurityConfig/SecurityConfig.java
+++ src/main/java/org/esfe/SecurityConfig/SecurityConfig.java
@@ -16,26 +16,26 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsManager customUsers(DataSource dataSource) {
-        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-        // Consulta para obtener el usuario, contraseña y nombre
+    JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+    //Consulta para obtener el usuario, contraseña y nombre
         users.setUsersByUsernameQuery("select correo, contraseña, status from Usuario where correo = ?");
         // Consulta para obtener los roles del usuario
         users.setAuthoritiesByUsernameQuery(
-                "select u.correo, r.nombre " +  //e.nombre || Selecciona el nombre del rol y del estado
-                        "from usuario_rol ur " +
+              "select u.correo, r.nombre " +  //e.nombre || Selecciona el nombre del rol y del estado
+                       "from usuario_rol ur " +
                         "inner join Usuario u on u.usuario_id = ur.usuario_id " +  // Une Usuario con usuario_rol
                         "inner join rol r on r.rol_id = ur.rol_id " +  // Une usuario_rol con Rol
-                        //"inner join Estado e on u.estado_id = e.estado_id " +  // Une Usuario con Estado
+                       // "inner join Estado e on u.estado_id = e.estado_id " +  // Une Usuario con Estado
                         "where u.correo = ?"
         );
         return users;
     }
 
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
+       http.authorizeHttpRequests(authorize -> authorize
                 // aperturar el acceso a los recursos estáticos
                 .requestMatchers("/assets/*", "/css/", "/js/*").permitAll()
-                // las vistas públicas no requieren autenticación
+              // las vistas públicas no requieren autenticación
                 .requestMatchers("/", "/privacy", "/terms").permitAll()
                 // todas las demás vistas requieren autenticación
                 .anyRequest().authenticated());
